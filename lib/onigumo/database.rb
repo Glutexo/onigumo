ef2dc570a53cdeb87eda22e7aac517e06d0bbac4
Štěpán Tomsa <stomsa@redhat.com>
2019-08-23 12:57:54 +0200

    Complete the schema
    Make the naïve Schema complete for now. Unify column names, add all
    tables.

--- lib/onigumo/database.rb
+++ lib/onigumo/database.rb
@@ -11,26 +11,34 @@ module Onigumo
     end
     
     def add_download(url)
-      @conn[:downloads] << {url: url.to_s, downloaded: 0}
+      @conn[:downloads] << {url: url.to_s, complete: 0}
     end
     
     def add_action(spider, meth)
-      @conn[:actions] << {spider: spider, method: meth, performed: 0}
+      @conn[:actions] << {spider: spider, method: meth, complete: 0}
     end
     
     private
     def init_schema
-      @conn.create_table(:downloads) do
-        primary_key(:id)
-        String(:url)
-        Integer(:downloaded)  # boolean
-      end
-      
       @conn.create_table(:actions) do
         primary_key(:id)
-        String(:spider)
-        String(:method)
-        Integer(:performed)  # boolean
+        String(:spider, null: false)
+        String(:method, null: false)
+        Integer(:complete, null: false)  # boolean
+      end
+
+      @conn.create_table(:downloads) do
+        primary_key(:id)
+        String(:url, null: false)
+        Integer(:complete, null: false)  # boolean
+      end
+
+      @conn.create_table(:parses) do
+        primary_key(:id)
+        foreign_key(:download, :downloads)
+        String(:spider, null: false)
+        String(:method, null: false)
+        Integer(:complete, null: false)  # boolean
       end
     end
   end
