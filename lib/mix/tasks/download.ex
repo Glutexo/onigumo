0e9dbbcc85f5a5f5a22577f49c66575a8d9d4472
Glutexo <glutexo@icloud.com>
2021-12-11 11:10:53 +0100

    Extract URL to a constant

--- lib/mix/tasks/download.ex
+++ lib/mix/tasks/download.ex
@@ -1,14 +1,14 @@
 defmodule Mix.Tasks.Download do
   use Mix.Task
 
+  @url "https://www.httpbin.org/html"
+
   def run(_) do
     HTTPoison.start()
     %HTTPoison.Response{
       status_code: 200,
       body: body,
-    } = HTTPoison.get!(
-      "https://www.httpbin.org/html"
-    )
+    } = HTTPoison.get!(@url)
     IO.puts("Got milk: #{body}")
   end
 end
