4c73e5fe09290e2043aa685d7263a32fbfbfdaa2
Glutexo <glutexo@icloud.com>
2021-12-11 11:32:25 +0100

    Move download code from Task to Onigumo

--- lib/mix/tasks/download.ex
+++ lib/mix/tasks/download.ex
@@ -1,16 +1,7 @@
 defmodule Mix.Tasks.Download do
   use Mix.Task
 
-  @url "https://www.httpbin.org/html"
-  @filename "body.html"
-
   def run(_) do
-    HTTPoison.start()
-    
-    %HTTPoison.Response{
-      status_code: 200,
-      body: body,
-    } = HTTPoison.get!(@url)
-    File.write!(@filename, body) 
+    Onigumo.download()
   end
 end
--- lib/onigumo.ex
+++ lib/onigumo.ex
@@ -1,18 +1,14 @@
 defmodule Onigumo do
-  @moduledoc """
-  Documentation for `Onigumo`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Onigumo.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @url "https://www.httpbin.org/html"
+  @filename "body.html"
+  
+  def download() do
+    HTTPoison.start()
+    
+    %HTTPoison.Response{
+      status_code: 200,
+      body: body,
+    } = HTTPoison.get!(@url)
+    File.write!(@filename, body)
   end
 end
