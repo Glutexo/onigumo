02455a339579c953142904943fde8f596ea744b3
Glutexo <glutexo@icloud.com>
2021-12-11 11:20:14 +0100

    Write response body to a file

--- lib/mix/tasks/download.ex
+++ lib/mix/tasks/download.ex
@@ -2,13 +2,15 @@ defmodule Mix.Tasks.Download do
   use Mix.Task
 
   @url "https://www.httpbin.org/html"
+  @filename "body.html"
 
   def run(_) do
     HTTPoison.start()
+    
     %HTTPoison.Response{
       status_code: 200,
       body: body,
     } = HTTPoison.get!(@url)
-    IO.puts("Got milk: #{body}")
+    File.write!(@filename, body) 
   end
 end
