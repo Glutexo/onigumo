c3d64a77ce80786d4650ff8fad73f2f903b9b002
dstroch <d.stroch@gmail.com>
2022-01-08 12:03:39 +0100

    Add test to download with args

--- test/onigumo_test.exs
+++ test/onigumo_test.exs
@@ -2,6 +2,7 @@ defmodule OnigumoTest do
   use ExUnit.Case
   import Mox
 
+  @url "http://onigumo.org/hello.html"
   @filename "body.html"
 
   setup(:verify_on_exit!)
@@ -10,15 +11,16 @@ defmodule OnigumoTest do
     expect(
       HTTPoisonMock,
       :get!,
-      fn _url ->
+      fn url ->
         %HTTPoison.Response{
           status_code: 200,
-          body: "hello\n"
+          body: "Body from: #{url}"
         }
       end
     )
 
-    assert(:ok = Onigumo.download())
-    assert("hello\n" = File.read!(@filename))
+    assert(:ok = Onigumo.download(HTTPoisonMock, @url))
+    assert("Body from: #{@url}" = File.read!(@filename))
   end
+
 end
