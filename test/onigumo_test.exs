75749f547a25a2db48d84dc71f7ab218e644419b
dstroch <d.stroch@gmail.com>
2022-01-21 19:36:05 +0100

    Add integration test and download multiple URLs

--- test/onigumo_test.exs
+++ test/onigumo_test.exs
@@ -17,17 +17,39 @@ defmodule OnigumoTest do
       fn url ->
         %HTTPoison.Response{
           status_code: 200,
-          body: "Body from: #{url}"
+          body: "Body from: #{url}\n"
         }
       end
     )
 
-    assert(:ok == Onigumo.download(HTTPoisonMock, @url_1, output_file))
-    assert("Body from: #{@url_1}" == File.read!(output_file))
+    assert(:ok == Onigumo.download(@url_1, HTTPoisonMock, output_file))
+    assert("Body from: #{@url_1}\n" == File.read!(output_file))
+  end
+
+
+  @tag :tmp_dir
+  test("download multiple URLs", %{tmp_dir: tmp_dir}) do
+    output_file = Path.join(tmp_dir, "outputfile")
+
+    expect(
+      HTTPoisonMock,
+      :get!,
+      2,
+      fn url ->
+        %HTTPoison.Response{
+          status_code: 200,
+          body: "Body from: #{url}\n"
+        }
+      end
+    )
+
+    urls = [@url_1, @url_2]
+    assert([:ok, :ok] == Onigumo.download(urls, HTTPoisonMock, output_file))
+    assert("Body from: #{@url_2}\n" == File.read!(output_file))
   end
 
   @tag :tmp_dir
-  test("download multiply URLs", %{tmp_dir: tmp_dir}) do
+  test("integration test", %{tmp_dir: tmp_dir}) do
     output_file = Path.join(tmp_dir, "outputfile")
     input_file = Path.join(tmp_dir, "inputfile")
 
@@ -45,10 +67,10 @@ defmodule OnigumoTest do
 
     content = "#{@url_1}\n#{@url_2}\n"
     File.write!(input_file, content)
-    expected = "Body from: #{@url_1}\nBody from: #{@url_2}\n"
+    expected = "Body from: #{@url_2}\n"
     # load urls from urls_file
     # read result from some test file
-    Onigumo.save_urls_contents(input_file, output_file)
+    Onigumo.save_urls_contents(HTTPoisonMock, input_file, output_file)
 
     assert(expected == File.read!(output_file))
   end
