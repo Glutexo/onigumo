d931b979cedbfa09b6ce7568aedee6659278b2a8
Glutexo <glutexo@icloud.com>
2022-01-22 11:49:06 +0100

    Test load multiple URLs

--- test/onigumo_test.exs
+++ test/onigumo_test.exs
@@ -2,7 +2,10 @@ defmodule OnigumoTest do
   use ExUnit.Case
   import Mox
 
-  @url "http://onigumo.org/hello.html"
+  @urls [
+    "http://onigumo.org/hello.html",
+    "http://onigumo.org/bye.html"
+  ]
   @filename "body.html"
   @testfile_with_urls "urls.txt"
 
@@ -20,19 +23,30 @@ defmodule OnigumoTest do
       end
     )
 
-    assert(:ok == Onigumo.download(HTTPoisonMock, @url))
-    assert("Body from: #{@url}" == File.read!(@filename))
+    url = Enum.at(@urls, 0)
+    assert(:ok == Onigumo.download(HTTPoisonMock, url))
+    assert("Body from: #{url}" == File.read!(@filename))
   end
 
 
   @tag :tmp_dir
-  test("load URL from file", %{tmp_dir: tmp_dir}) do
+  test("load a single URL form a file", %{tmp_dir: tmp_dir}) do
+    url = Enum.at(@urls, 0)
+    
     filepath = Path.join(tmp_dir, @testfile_with_urls)
-    content = @url <> " \n"
+    content = url <> " \n"
     File.write!(filepath, content)
 
-    expected = [@url]
+    expected = [url]
     assert(expected == Onigumo.load_urls(filepath))
   end
 
+  @tag :tmp_dir
+  test("load multiple URLs from a file", %{tmp_dir: tmp_dir}) do
+    filepath = Path.join(tmp_dir, @testfile_with_urls)
+    content = Enum.map(@urls, &(&1 <> " \n")) |> Enum.join()
+    File.write!(filepath, content)
+
+    assert(@urls == Onigumo.load_urls(filepath))
+  end
 end
