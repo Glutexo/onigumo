a02555955ecced2fb0b0dc0231446d37879492af
Glutexo <glutexo@icloud.com>
2022-08-01 21:04:44 +0200

    wip try Application

--- lib/onigumo_cli.ex
+++ lib/onigumo_cli.ex
@@ -1,4 +1,11 @@
 defmodule Onigumo.CLI do
+  use Application
+
+  @impl Application
+  def start(_type, _args) do
+    Task.start(Onigumo, :main, [])
+  end
+
   def main(_args) do
     Onigumo.main()
   end
--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule Onigumo.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Onigumo.CLI, []}
     ]
   end
 
