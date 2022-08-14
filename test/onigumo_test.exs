39b343dae6be04a76a5e5f2d97289f734d72b83e
Glutexo <glutexo@icloud.com>
2022-08-14 21:47:46 +0200

    Make tests order match the tested module

--- test/onigumo_test.exs
+++ test/onigumo_test.exs
@@ -10,21 +10,6 @@ defmodule OnigumoTest do
   setup(:verify_on_exit!)
 
   @tag :tmp_dir
-  test("download a URL", %{tmp_dir: tmp_dir}) do
-    expect(HTTPoisonMock, :get!, &prepare_response/1)
-
-    input_url = Enum.at(@urls, 0)
-    download_result = Onigumo.download_url(input_url, tmp_dir)
-    assert(download_result == :ok)
-
-    output_file_name = Onigumo.create_file_name(input_url)
-    output_path = Path.join(tmp_dir, output_file_name)
-    read_output = File.read!(output_path)
-    expected_output = body(input_url)
-    assert(read_output == expected_output)
-  end
-
-  @tag :tmp_dir
   test("download URLs from the input file", %{tmp_dir: tmp_dir}) do
     expect(HTTPoisonMock, :get!, length(@urls), &prepare_response/1)
 
@@ -39,27 +24,18 @@ defmodule OnigumoTest do
   end
 
   @tag :tmp_dir
-  test("load a single URL from a file", %{tmp_dir: tmp_dir}) do
-    input_urls = Enum.slice(@urls, 0, 1)
+  test("download a URL", %{tmp_dir: tmp_dir}) do
+    expect(HTTPoisonMock, :get!, &prepare_response/1)
 
-    input_path_env = Application.get_env(:onigumo, :input_path)
-    input_path_tmp = Path.join(tmp_dir, input_path_env)
-    input_file_content = prepare_input(input_urls)
-    File.write!(input_path_tmp, input_file_content)
+    input_url = Enum.at(@urls, 0)
+    download_result = Onigumo.download_url(input_url, tmp_dir)
+    assert(download_result == :ok)
 
-    loaded_urls = Onigumo.load_urls(tmp_dir) |> Enum.to_list()
-    assert(loaded_urls == input_urls)
-  end
-
-  @tag :tmp_dir
-  test("load multiple URLs from a file", %{tmp_dir: tmp_dir}) do
-    input_path_env = Application.get_env(:onigumo, :input_path)
-    input_path_tmp = Path.join(tmp_dir, input_path_env)
-    input_file_content = prepare_input(@urls)
-    File.write!(input_path_tmp, input_file_content)
-
-    loaded_urls = Onigumo.load_urls(tmp_dir) |> Enum.to_list()
-    assert(loaded_urls == @urls)
+    output_file_name = Onigumo.create_file_name(input_url)
+    output_path = Path.join(tmp_dir, output_file_name)
+    read_output = File.read!(output_path)
+    expected_output = body(input_url)
+    assert(read_output == expected_output)
   end
 
   test("get response by HTTP request") do
@@ -88,6 +64,30 @@ defmodule OnigumoTest do
 
     read_output = File.read!(output_path)
     assert(read_output == response)
+  end
+
+  @tag :tmp_dir
+  test("load a single URL from a file", %{tmp_dir: tmp_dir}) do
+    input_urls = Enum.slice(@urls, 0, 1)
+
+    input_path_env = Application.get_env(:onigumo, :input_path)
+    input_path_tmp = Path.join(tmp_dir, input_path_env)
+    input_file_content = prepare_input(input_urls)
+    File.write!(input_path_tmp, input_file_content)
+
+    loaded_urls = Onigumo.load_urls(tmp_dir) |> Enum.to_list()
+    assert(loaded_urls == input_urls)
+  end
+
+  @tag :tmp_dir
+  test("load multiple URLs from a file", %{tmp_dir: tmp_dir}) do
+    input_path_env = Application.get_env(:onigumo, :input_path)
+    input_path_tmp = Path.join(tmp_dir, input_path_env)
+    input_file_content = prepare_input(@urls)
+    File.write!(input_path_tmp, input_file_content)
+
+    loaded_urls = Onigumo.load_urls(tmp_dir) |> Enum.to_list()
+    assert(loaded_urls == @urls)
   end
 
   test("create file name from URL") do
