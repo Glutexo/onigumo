7d55762b87e019d0bdaaa54bb1a41610b5cce86a
dstroch <d.stroch@gmail.com>
2023-01-21 20:31:36 +0100

    Move parser.ex to spider_html.ex
        - tool for finding in html is not part of onigumo Kernel Parser
        - it is handy tool for spiders, most of spiders will need some
        basic html url finder, it makes senses to prepare some basic func
        for that.

--- lib/spider_html.ex
+++ lib/spider_html.ex
@@ -1,5 +1,5 @@
-defmodule Parser do
-  def html_links(document) do
+defmodule Spider.HTML do
+  def find_links(document) do
     Floki.parse_document!(document)
     |> Floki.find("a")
     |> Floki.attribute("href")
