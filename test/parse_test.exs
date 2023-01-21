6422b0390a8206a7ae7126269cbad3b7ff710a17
dstroch <d.stroch@gmail.com>
2023-01-21 21:01:39 +0100

    Fix sigil content, indentation and remove double qoutes

--- test/parse_test.exs
+++ test/parse_test.exs
@@ -5,17 +5,17 @@ defmodule ParseTest do
     "http://onigumo.local/hello.html",
     "http://onigumo.local/bye.html"
   ]
-  @html ~s("<!doctype html>
-      <html>
-      <body>
+  @html ~s(<!doctype html>
+<html>
+    <body>
         <section id="content">
-          <p class="headline">Floki</p>
-          <a href="http://onigumo.local/hello.html">Hello</a>
-          <a href="http://onigumo.local/bye.html">Bye</a>
-          <span data-model="user">onigumo</span>
+            <p class="headline">Floki</p>
+            <a href="http://onigumo.local/hello.html">Hello</a>
+            <a href="http://onigumo.local/bye.html">Bye</a>
+            <span data-model="user">onigumo</span>
         </section>
-      </body>
-      </html>")
+    </body>
+</html>)
 
   test("Parsing values of href attributes in html links") do
     result = Parser.html_links(@html)
