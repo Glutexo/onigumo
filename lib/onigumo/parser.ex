8cf202eabb3eb6a730d8daffbad8aba9711a4ef9
Glutexo <glutexo@icloud.com>
2023-05-20 19:50:43 +0200

    Extract file listing to methods

--- lib/onigumo/parser.ex
+++ lib/onigumo/parser.ex
@@ -5,8 +5,18 @@ defmodule Onigumo.Parser do
 
   def main(root_path) do
     root_path
-    |> File.ls!()
-	|> Enum.filter(fn filename -> Path.extname(filename) == ".raw" end)
+	|> list_downloaded()
     |> Enum.map(&IO.puts(&1))
+  end
+
+  defp list_downloaded(path) do
+  	path
+    |> File.ls!()
+	|> Enum.filter(&is_downloaded(&1))
+  end
+
+  defp is_downloaded(path) do
+    suffix = Application.get_env(:onigumo, :downloaded_suffix)
+    Path.extname(path) == ".#{suffix}"
   end
 end
