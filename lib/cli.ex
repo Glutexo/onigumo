3d4cca5b8493a458f3ad99dd01ed465140a1a229
dstroch <d.stroch@gmail.com>
2023-06-09 11:23:29 +0200

    Parse switches output and component from CLI
       - parse output path and component to run as
       switch -o and --component

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,7 +1,28 @@
 defmodule Onigumo.CLI do
-  def main([component]) do
-    module = Module.safe_concat("Onigumo", component)
-    root_path = File.cwd!()
-    module.main(root_path)
+  def main(argv) do
+    {parsed_switches, _, _} =
+      argv
+      |> parse_argv
+
+    module =
+      parsed_switches
+      |> Keyword.get(:component, "Downloader")
+      |> safe_concat()
+
+    parsed_switches
+    |> Keyword.get(:output, File.cwd!())
+    |> module.main()
+  end
+
+  defp parse_argv(argv) do
+      argv
+      |> OptionParser.parse(
+        aliases: [o: :output],
+        strict: [component: :string, output: :string]
+      )
+  end
+
+  defp safe_concat(component) do
+    Module.safe_concat("Onigumo", component)
   end
 end
