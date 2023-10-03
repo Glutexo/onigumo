9f789ca8d8ef3e586218f348b9280230c2e388bd
dstroch <d.stroch@gmail.com>
2023-10-03 10:38:50 +0200

    Create module from parsed component
       parse component as first position argument from CLI.
       Rest of positional arguments are unused. This goal is
       achieved by pattern matching of private func get_component.
       If no positional argument is specified then "Downloader"
       must be used.

--- lib/cli.ex
+++ lib/cli.ex
@@ -2,7 +2,19 @@ defmodule Onigumo.CLI do
   def main(argv) do
     {_parsed, args, _invalid} = OptionParser.parse(argv, strict: [])
 
+    module =
+      args
+      |> get_component
+      |> safe_concat
+
     root_path = File.cwd!()
     module.main(root_path)
+  end
+
+  defp get_component([]), do: "Downloader"
+  defp get_component([component | _]), do: component
+
+  defp safe_concat(component) do
+    Module.safe_concat("Onigumo", component)
   end
 end
