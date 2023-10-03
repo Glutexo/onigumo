fb6665f4bc44e1b791a357f2a6e0220bb192f3fb
dstroch <d.stroch@gmail.com>
2023-10-03 10:36:19 +0200

    Use OptionParser to parse component
       we have to specify option strict: with empty list,
       otherwise we get an error from elixir:
          "warning: not passing the :switches or :strict option to OptionParser is deprecate"

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,6 +1,7 @@
 defmodule Onigumo.CLI do
-  def main([component]) do
-    module = Module.safe_concat("Onigumo", component)
+  def main(argv) do
+    {_parsed, args, _invalid} = OptionParser.parse(argv, strict: [])
+
     root_path = File.cwd!()
     module.main(root_path)
   end
