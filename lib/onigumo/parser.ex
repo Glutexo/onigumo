7223f367c33b019bb7aafcfc527a29f5084fe0ec
Glutexo <glutexo@icloud.com>
2023-10-21 20:01:17 +0200

    Fix indentation
    Replace tabs with spaces.

--- lib/onigumo/parser.ex
+++ lib/onigumo/parser.ex
@@ -5,14 +5,14 @@ defmodule Onigumo.Parser do
 
   def main(root_path) do
     root_path
-	|> list_downloaded()
+    |> list_downloaded()
     |> Enum.map(&IO.puts(&1))
   end
 
   defp list_downloaded(path) do
-  	path
+    path
     |> File.ls!()
-	|> Enum.filter(&is_downloaded(&1))
+    |> Enum.filter(&is_downloaded(&1))
   end
 
   defp is_downloaded(path) do
