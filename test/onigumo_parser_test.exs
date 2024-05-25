8895452a58e498c768f511c6bdc73c2e8a990993
Glutexo <glutexo@icloud.com>
2024-05-25 17:13:33 +0200

    Add main result test
    The Onigumo.Component behavior requires main to return :ok.

--- test/onigumo_parser_test.exs
+++ test/onigumo_parser_test.exs
@@ -7,8 +7,14 @@ defmodule OnigumoParserTest do
   describe("Onigumo.Parser.main/1") do
     @tag :tmp_dir
     test("run Parser", %{tmp_dir: tmp_dir}) do
+      _result = Onigumo.Parser.main(tmp_dir)
+      assert(false)  # TODO: Do!
+    end
+
+    @tag :tmp_dir
+    test("return :ok", %{tmp_dir: tmp_dir}) do
       result = Onigumo.Parser.main(tmp_dir)
-      assert(result)
+      assert(result == :ok)
     end
   end
 end
