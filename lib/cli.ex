68fa8f94f3faea76d655c2d1eee871c96f1f0094
dstroch <d.stroch@gmail.com>
2025-01-04 20:49:21 +0100

    Just a example

--- lib/cli.ex
+++ lib/cli.ex
@@ -4,16 +4,43 @@ defmodule Onigumo.CLI do
   }
 
   def main(argv) do
-    parsed = OptionParser.parse(argv, aliases: [C: :working_dir], strict: [working_dir: :string])
+    parsed =
+      OptionParser.parse(
+        argv,
+        aliases: [h: :help, C: :working_dir],
+        strict: [help: :boolean, working_dir: :string]
+      )
 
     with {switches, [component], []} <- parsed,
          {:ok, module} <- Map.fetch(@components, String.to_atom(component)) do
       working_dir = Keyword.get(switches, :working_dir, File.cwd!())
       module.main(working_dir)
     else
+      {[help: true], [], []} -> usage_message()
       {_, _, [_ | _]} -> usage_message()
       {_, argv, _} when length(argv) != 1 -> usage_message()
       :error -> usage_message()
+    end
+
+    case parsed do
+      {[help: true], [], []} ->
+        usage_message()
+
+      {_, _, [_ | _]} ->
+        usage_message()
+
+      {_, argv, _} when length(argv) != 1 ->
+        usage_message()
+
+      {switches, [component], []} ->
+        case Map.fetch(@components, String.to_atom(component)) do
+          {:ok, module} ->
+            working_dir = Keyword.get(switches, :working_dir, File.cwd!())
+            module.main(working_dir)
+
+          :error ->
+            usage_message()
+        end
     end
   end
 
