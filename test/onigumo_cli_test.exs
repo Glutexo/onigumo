86d594b2481167dd5409e5fcaa768bc017d15142
David Štroch <45515588+nappex@users.noreply.github.com>
2025-04-01 18:51:32 +0200

    Remove whitespace
    Co-authored-by: Glutexo <driezasson@me.com>

--- test/onigumo_cli_test.exs
+++ test/onigumo_cli_test.exs
@@ -51,7 +51,7 @@ defmodule OnigumoCLITest do
     end
 
     for combination <- @invalid_combinations do
-      test("run CLI with invalid combination #{inspect(combination)} ") do
+      test("run CLI with invalid combination #{inspect(combination)}") do
         assert usage_message_printed?(fn -> Onigumo.CLI.main(unquote(combination)) end)
       end
     end
