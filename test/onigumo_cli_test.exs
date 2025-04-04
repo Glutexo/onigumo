1012b4633d15789ccda8927e260bdf4a31dfd879
dstroch <d.stroch@gmail.com>
2025-04-05 01:57:39 +0200

    Fix formatting

--- test/onigumo_cli_test.exs
+++ test/onigumo_cli_test.exs
@@ -83,15 +83,15 @@ defmodule OnigumoCLITest do
       end
     end
 
-      test("Usage message ends with one newline") do
-        output = capture_io(fn -> Onigumo.CLI.main([]) end)
-        assert not String.ends_with?(output, "\n\n")
-      end
+    test("Usage message ends with one newline") do
+      output = capture_io(fn -> Onigumo.CLI.main([]) end)
+      assert not String.ends_with?(output, "\n\n")
+    end
 
-      test("Help message ends with one newline") do
-        output = capture_io(fn -> Onigumo.CLI.main(["-h"]) end)
-        assert not String.ends_with?(output, "\n\n")
-      end
+    test("Help message ends with one newline") do
+      output = capture_io(fn -> Onigumo.CLI.main(["-h"]) end)
+      assert not String.ends_with?(output, "\n\n")
+    end
 
     defp usage_message_printed?(function) do
       output = capture_io(function)
